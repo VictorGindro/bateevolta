1688fe065611dc8afe4479b19a55d598df9d84af
Victor Gindro -X (vgindro - CIEE at Cisco) <vgindro@cisco.com>
2019-08-27 17:42:33 -0300

    added date to the card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,31 +1,35 @@
-<template>
-    <v-card elevation="24" max-width="220" color="rgb(195,222,233)">
+ <template>
+    <v-card elevation="24" max-width="225" color="rgb(195,222,233)">
         <v-system-bar color="rgb(247,245,215)" lights-on height="50" class="justify-center">
-            <v-btn icon="supervisor_account"  color="white">
+            <v-btn icon="supervisor_account" color="white">
                 <v-icon color="rgb(70, 180, 199)">supervisor_account</v-icon>
             </v-btn>
             <!-- <v-spacer/> -->
             <div>Kevin</div>
-            <v-spacer/>
-            <div>17/09/2020</div>
-        </v-system-bar >
-        <v-list two-line>
-            <v-img src="https://wallpaperbro.com/img/212940.jpg">
-            <v-list-item dark>
-                <v-list-item-avatar>
-                    <v-btn class="mx-2" fab small color="white">
-                        <v-icon color="rgb(70, 180, 199)">directions_bus</v-icon>
-                    </v-btn>
-                </v-list-item-avatar>
-                <v-list-item-content>
-                    <v-list-item-title class="fill-height" align="center" justify="center">Santos</v-list-item-title>
-                    <v-list-item-subtitle  class="fill-height" align="center" justify="center">Praia Grande
-                    </v-list-item-subtitle>
-                </v-list-item-content>
-                <v-list-item-action>
-                </v-list-item-action>
-            </v-list-item>
-            </v-img>
+            <v-spacer />
+            <div class="mt-1">                   
+                <v-list-item-subtitle class="fill-height">Ida: 16/12/2020</v-list-item-subtitle>
+                <v-list-item-subtitle class="fill-height">Volta: 19/12/2020</v-list-item-subtitle>
+
+            </div>
+        </v-system-bar>
+        <v-list two-line  >
+            <v-img aspect-ratio="2.00" src="https://wallpaperbro.com/img/212940.jpg">
+                <v-list-item>
+                    <v-list-item-avatar>
+                        <v-btn class="mx-2" fab small color="white">
+                            <v-icon color="rgb(70, 180, 199)">directions_bus</v-icon>
+                        </v-btn>
+                    </v-list-item-avatar>
+                    <v-list-item-content>
+                        <v-list-item-title class="fill-height" align="center" justify="center">Santos
+                        </v-list-item-title>
+                        <v-list-item-title class="fill-height" align="center" justify="center">Praia Grande</v-list-item-title>
+                    </v-list-item-content>
+                    <v-list-item-action>
+                    </v-list-item-action>
+                </v-list-item>
+             </v-img>
         </v-list>
     </v-card>
 </template>
--- src/pages/List.vue
+++ src/pages/List.vue
@@ -4,9 +4,9 @@
       <v-img :src="require('../assets/BeV/f1-w.png')" class="mb-3"  contain height="150"></v-img>
       <h2 class="headline font-weight-bold mb-3">Suas Viagens:</h2>
     </div>
-    <v-layout text-center wrap class="mb-5 pb-5">
-      <v-col  v-for="n in 15" :key="n">
-        <Card />
+    <v-layout text-center wrap class="mb-5 ml-5 pb-5">
+      <v-col class="ml-5" v-for="n in 6" :key="n">
+        <Card class="ma-1" />
       </v-col>
     </v-layout>
     <v-bottom-navigation :value="activeBtn" light grow color="rgb(70, 180, 199)">
