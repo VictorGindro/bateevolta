c33d73e8eb872157738ad23218513b4743a2cdcc
Victor Gindro -X (vgindro - CIEE at Cisco) <vgindro@cisco.com>
2019-10-09 18:33:34 -0300

    chencged the app bar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,57 @@
 <template>
   <v-app>
-    <v-app-bar app clipped-left color='rgb(51, 204, 204)' dark >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer " v-if=" this.$router.history.current.path !='/home'"></v-app-bar-nav-icon>
-      <v-img><v-btn icon @click="home">
-        <v-icon>home</v-icon>
-      </v-btn></v-img>
+    <v-app-bar app clipped-left color='rgb(51, 204, 204)' dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer " v-if=" this.$router.history.current.path !='/home'">
+      </v-app-bar-nav-icon>
+      <v-img>
+        <v-btn icon @click="home">
+          <v-icon>home</v-icon>
+        </v-btn>
+      </v-img>
+      <v-toolbar-title></v-toolbar-title>
       <div class="flex-grow-1"></div>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+
+      <v-menu offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn icon dark v-on="on">
+            <v-icon>person</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item>
+            <v-list-item-title @click="login()">
+              <v-btn>Login</v-btn>
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
-            <v-navigation-drawer v-if=" this.$router.history.current.path !='/home'" color="rgb(41, 163, 163)" v-model="drawer" app clipped dark>
-            <v-list dense>
-                <v-list-item @click="compras()">
-                    <v-list-item-action>
-                        <v-icon>calendar_today</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                        <v-list-item-title >Minhas compras</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-                <v-list-item @click="diponiveis()">
-                    <v-list-item-action>
-                        <v-icon>list</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                        <v-list-item-title >Viagens Disponiveis</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-list>
-        </v-navigation-drawer>
+    <v-navigation-drawer v-if=" this.$router.history.current.path !='/home'" color="rgb(41, 163, 163)" v-model="drawer"
+      app clipped dark>
+      <v-list dense>
+        <v-list-item @click="compras()">
+          <v-list-item-action>
+            <v-icon>calendar_today</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Minhas compras</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item @click="diponiveis()">
+          <v-list-item-action>
+            <v-icon>list</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Viagens Disponiveis</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-content>
       <router-view></router-view>
     </v-content>
@@ -42,6 +67,10 @@ export default {
   },
   data: () => ({
     drawer: false,
+    fab:  false,
+    items: [
+        { title: 'Login' },
+      ],
   }),
       methods:{
       compras(){
@@ -52,6 +81,9 @@ export default {
       },
       home(){
         this.$router.push("/home");
+      },
+      login(){
+          this.$router.push("/login")
       }
     }
 };
